f8812bd718f7c3081c21ca94c93d66b386c216f1
Darshan <darshankumaravelk@gmail.com>
2025-03-13 21:26:06 +0530

    finished dsekptop working on mobile

---
+++ index.js
@@ -0,0 +1,25 @@
+let roleTyping = new Typed(".typing-role",{
+    strings:['Web Developer'],
+    typeSpeed:150,
+    backSpeed:100,
+    loop:true   
+})
+
+function setLink() {
+    const links = [
+   document.getElementById("home"),
+     document.getElementById("TechStack"),
+     document.getElementById("Projects"),
+    document.getElementById("contact")]
+    const hash = window.location.hash || "#home";
+    for(let link of links) {
+        link.classList.remove('active');
+        if(link.getAttribute("href") === hash){
+            link.classList.add("active");
+        }
+    }
+
+}
+
+window.addEventListener("hashchange" , setLink);
+window.addEventListener("load" , setLink);
